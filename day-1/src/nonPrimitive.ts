37fe13b662b440b3a3ce4371036c249e62e102c5
Mysterio-O <skrabbi.019@gmail.com>
2025-11-09 19:09:04 +0600

    added non primitive

--- day-1/src/nonPrimitive.ts
+++ day-1/src/nonPrimitive.ts
@@ -8,4 +8,20 @@ console.log(arrOfStr);
 // tuple
 
 let userInfo: [string, number] = ['user', 10];
-let activities: { a: string, b: number, c: undefined } = { a: 'string', b: 10, c: undefined }
+let activities: { a: string, b: number, c: undefined } = { a: 'string', b: 10, c: undefined };
+
+const user: {
+    first_name: string;
+    middle_name: string;
+    last_name: string;
+    readonly literal:string;
+} = {
+    first_name: 'John',
+    middle_name: 'stormborn',
+    last_name: 'smith',
+    literal:'Literal'
+};
+
+user.literal= "Literal Literal"
+
+console.log(user)
