d0fe2299701089be7c610a3c745573fb37b73da3
Mysterio-O <skrabbi.019@gmail.com>
2025-11-09 18:56:44 +0600

    added non primitive

---
+++ day-1/src/nonPrimitive.ts
@@ -0,0 +1,11 @@
+let arrOfStr: string[] = ['st', 'eds', 'dg'];
+
+arrOfStr.push('fd');
+arrOfStr.push('eee');
+console.log(arrOfStr);
+
+
+// tuple
+
+let userInfo: [string, number] = ['user', 10];
+let activities: { a: string, b: number, c: undefined } = { a: 'string', b: 10, c: undefined }
---
+++ day-1/src/primitive.ts
@@ -0,0 +1,4 @@
+// ? types in typescript
+// * string, number, boolean, undefined, null, never, unknown, void
+
+let str = 'string string';
