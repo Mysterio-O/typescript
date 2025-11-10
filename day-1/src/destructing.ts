c4de35b07ca9cca03ae54b42c3391369c3582573
Mysterio-O <skrabbi.019@gmail.com>
2025-11-10 17:16:56 +0600

    added type alias

--- day-1/src/destructing.ts
+++ day-1/src/destructing.ts
@@ -1,10 +1,12 @@
 const user = {
-    id:123,
-    name:{
-        firstName:'johnny',
-        middleName:'johnny',
-        lastName:'johnny'
+    id: 123,
+    name: {
+        firstName: 'johnny',
+        middleName: 'johnny',
+        lastName: 'johnny'
     },
-    gender:'male',
-    favColor:'black'
+    gender: 'male',
+    favColor: 'black'
 }
+
+const { id, name: { middleName } } = user;
---
+++ day-1/src/typeAlias.ts
@@ -0,0 +1,19 @@
+const user1: {
+    id: number;
+    name: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+    },
+    gender: 'male' | 'female';
+    favColor: string;
+} = {
+    id: 123,
+    name: {
+        firstName: 'johnny',
+        middleName: 'johnny',
+        lastName: 'johnny'
+    },
+    gender: 'male',
+    favColor: 'black'
+}
